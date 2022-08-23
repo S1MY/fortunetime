cf951749a2cf2d0a81b0c6721dbfce2e26c2425b
S1MY <71487094+S1MY@users.noreply.github.com>
2022-08-24 00:13:36 +0500

    Update FreekassaController.php

--- app/Http/Controllers/FreekassaController.php
+++ app/Http/Controllers/FreekassaController.php
@@ -34,7 +34,7 @@ class FreekassaController extends Controller
         return 'https://pay.freekassa.ru/?m='.$merchant_id.'&oa='.$order_amount.'&currency='.$currency.'&o='.$order_id.'&s='.$s;
     }
     public function successful(Request $request){
-        dd($request);
+        // dd($request);
         return redirect()->route('account');
     }
 }
