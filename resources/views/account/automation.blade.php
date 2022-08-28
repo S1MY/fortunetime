68e31dad1ffa214b03d89853ff8e5e3909b2c64b
S1MY <71487094+S1MY@users.noreply.github.com>
2022-08-28 16:22:56 +0500

    Update automation.blade.php

--- resources/views/account/automation.blade.php
+++ resources/views/account/automation.blade.php
@@ -10,14 +10,16 @@
                 <p class="cabinetText">Система авторекрута под ключ. Полностью автоматизированная система, которая позволяет поставить ваш бизнес на автомат уже в первый день её применения.</p>
                 <div class="automatFlex displayFlex spaceBetween">
                     <div class="automatVideo">
-                        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <video name="media" autoplay controls style="width: 100%;">
+                            <source src="https://fortune-time.ru/video/Система финал.mp4" type="video/mp4">
+                        </video>
                     </div>
-                    <div class="automatLinks">
+                    {{-- <div class="automatLinks">
                         <a href="/" class="automatLink">Ссылка 1</a>
                         <a href="/" class="automatLink">Ссылка 2</a>
                         <a href="/" class="automatLink">Ссылка 3</a>
                         <a href="/" class="automatLink">Ссылка 4</a>
-                    </div>
+                    </div> --}}
                 </div>
             </div>
         </div>
