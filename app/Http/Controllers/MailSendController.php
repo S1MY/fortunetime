cb47f1118eee9f25db0adb20fc7b64bb08d4f626
S1MY <71487094+S1MY@users.noreply.github.com>
2022-09-21 22:56:59 +0500

    surname

--- app/Http/Controllers/MailSendController.php
+++ app/Http/Controllers/MailSendController.php
@@ -11,22 +11,19 @@ use function PHPUnit\Framework\isNull;
 class MailSendController extends Controller
 {
     public function sendMailContact(MailRequest $request){
+        if( isNull($request->forwhat) )
+        {
+            $name = $request->name;
+            $email = $request->email;
+            $question = $request->question;
 
-        dd($request);
+            $supportMail = 'S1MY.PJ@yandex.ru';
+            Mail::to($supportMail)->send(new SendMail($name, $email, $question));
+        }else{
+            return 'fucking spammers';
+        }
 
-        // if( isNull($request->forwhat) )
-        // {
-        //     $name = $request->name;
-        //     $email = $request->email;
-        //     $question = $request->question;
-
-        //     $supportMail = 'S1MY.PJ@yandex.ru';
-        //     Mail::to($supportMail)->send(new SendMail($name, $email, $question));
-        // }else{
-        //     return 'fucking spammers';
-        // }
-
-        // return true;
+        return true;
     }
 
 }
