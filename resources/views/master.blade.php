c75a147482eda914615abf0afd05aa5519803264
S1MY <71487094+S1MY@users.noreply.github.com>
2022-09-21 22:31:40 +0500

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -11,26 +11,6 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" integrity="sha512-+EoPw+Fiwh6eSeRK7zwIKG2MA8i3rV/DGa3tdttQGgWyatG/SkncT53KHQaS5Jh9MNOT3dmFL0FjTY08And/Cw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="/css/main.css">
         <script src="/js/jquery-3.6.0.min.js"></script>
-        <script type="text/javascript">
-            function callbackThen(response){
-                // read HTTP status
-                    console.log(response.status);
-
-                // read Promise object
-                response.json().then(function(data){
-                    console.log(data);
-                });
-            }
-            function callbackCatch(error){
-                console.error('Error:', error)
-            }
-        </script>
-        ...
-        {!! htmlScriptTagJsApiV3([
-            'action' => 'homepage',
-            'callback_then' => 'callbackThen',
-            'callback_catch' => 'callbackCatch'
-        ]) !!}
     </head>
     <body>
         <div class="page">
