a6e5414e399ab07ad18bc48c09488ff21d1e0b27
S1MY <71487094+S1MY@users.noreply.github.com>
2022-09-24 17:25:43 +0500

    Update FreekassaController.php

--- app/Http/Controllers/FreekassaController.php
+++ app/Http/Controllers/FreekassaController.php
@@ -6,6 +6,7 @@ use App\Models\Freekassa;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class FreekassaController extends Controller
 {
@@ -18,9 +19,22 @@ class FreekassaController extends Controller
 
         if (!in_array(getIP(), array('168.119.157.136', '168.119.60.227', '138.201.88.124', '178.154.197.79'))) die("hacking attempt!");
 
+        // Меняем статус оплаты
+
         Freekassa::where('id', $request->MERCHANT_ORDER_ID)->update([
             'remoteADDR' => $_SERVER['REMOTE_ADDR'],
             'status' => 1,
+        ]);
+
+        // Берём пользователя и создаём запись в связующей таблице
+
+        $fk_order = Freekassa::where('id', $request->MERCHANT_ORDER_ID)->first();
+        $user = User::where('id', $fk_order['user_id'])->first();
+
+        DB::table('matrix')->insert([
+            'user_id' => $user['user_id'],
+            'matrix_lvl' => 1,
+            'matrix_active' => 1
         ]);
 
     }
