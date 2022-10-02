c6c65bd3a44acd332dc0662988f6e62cc155368f
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-02 21:47:05 +0500

    Update main.blade.php

--- resources/views/account/main.blade.php
+++ resources/views/account/main.blade.php
@@ -106,7 +106,7 @@
                                                         <p class="pageTableUsername">{{$matrixInfo->login}}</p>
                                                         <p class="pageTableDate">{{$matrixInfo->email}}</p>
                                                     </div>
-                                                    <p class="matrixAddDate">{{ date($matrixInfo->created_at, 'YYYY-MM-DD')}}</p>
+                                                    <p class="matrixAddDate">{{ $matrixInfo->created_at->format('Y-m-d') }}</p>
                                                 </div>
                                             @endforeach
 
