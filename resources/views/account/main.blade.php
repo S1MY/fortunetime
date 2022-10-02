4ef5f4304b6a5dad9ee8e156cf22b010a2ba790c
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-02 22:53:20 +0500

    Update main.blade.php

--- resources/views/account/main.blade.php
+++ resources/views/account/main.blade.php
@@ -121,7 +121,7 @@
                                     $neeedly = $neeedly * $i;
                                 @endphp
                                 <div class="cabMatrixElement active">
-                                    <h3 class="cabMatrixName">Партнеры первого уровня <span>({{ $lineMatrixCounter }})</span></h3>
+                                    <h3 class="cabMatrixName">Партнеры первого уровня <span>({{  }})</span></h3>
                                     <p class="cabMatrixDesc">Необходимое количество участников в каждом плече для закрытия уровня - {{$neeedly}}</p>
                                     <div class="matrixFlex displayFlex spaceBetween">
                                         <div class="cabMatrixItem">
