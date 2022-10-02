25b572544f2d146409be952e34f4dd3d6f28f080
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-02 21:21:08 +0500

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -92,9 +92,16 @@ class MainController extends Controller
 
         $matrixInfos = DB::table('matrix_placers')->where('matrix_id', $matrix->matrix_id)->get();
 
+        $matrixInfos->toArray();
+
+        dd($matrixInfos);
+
         $matrixUsersCount = $matrixInfos->count();
 
-        dd($matrixUsersCount);
+        // for ($i=0; $i < $matrixUsersCount ; $i++) {
+        //     array_push($matrixInfos[$i], 'сюда');
+        // }
+        // dd($matrixUsersCount);
 
         return view('account.main', compact('matrix', 'disabled', 'matrixInfos'));
     }
