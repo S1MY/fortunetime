ca060701028afcfa8310531401274975e98ad6a6
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-02 21:06:41 +0500

    Update main.blade.php

--- resources/views/account/main.blade.php
+++ resources/views/account/main.blade.php
@@ -91,7 +91,7 @@
                 @if ( $matrix != null )
                     @if ($matrix->matrix_id != null)
                         {{-- Если у пользователя кто-то запустил матрицу --}}
-                        <div class="matrixElement" data-matrix="2">
+                        <div class="matrixElement" data-matrix="1">
                             <div class="cabMatrixElement active">
                                 <h3 class="cabMatrixName">Партнеры первого уровня <span>(3)</span></h3>
                                 <p class="cabMatrixDesc">Необходимое количество участников в каждом плече для закрытия уровня - 2</p>
