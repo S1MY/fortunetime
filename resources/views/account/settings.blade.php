a7cbe672864ee3930112be7708fe6fcce9501e3d
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-06 00:01:05 +0500

    Update settings.blade.php

--- resources/views/account/settings.blade.php
+++ resources/views/account/settings.blade.php
@@ -65,7 +65,7 @@
                             <p style="margin-bottom: 25px;">Личные данные</p>
                             <form class="passChange displayFlex spaceBetween AJAXForm" id="presonal" method="POST" action="{{ route('update.personal', Auth::user()->id) }}">
                                 @csrf
-                                <input type="text"{{$style}} class="formInput" placeholder="Введите ваше Имя" name="name" value="{{ Auth::user()->UserInfo->user_name }}" {{$readonly}}>
+                                <input type="text"{!$style!} class="formInput" placeholder="Введите ваше Имя" name="name" value="{{ Auth::user()->UserInfo->user_name }}" {{$readonly}}>
                                 <input type="password" class="formInput" placeholder="Введите вашу Фамилию" name="surname">
                                 <button class="passBtn" style="width: 100%;">Установить данные</button>
                             </form>
