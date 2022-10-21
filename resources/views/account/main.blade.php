3dd8e52dce460ce1c895c411acb7914c34304142
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-21 19:12:26 +0500

    Update main.blade.php

--- resources/views/account/main.blade.php
+++ resources/views/account/main.blade.php
@@ -73,7 +73,7 @@
                         $rpos++;
                     }
                     echo '<br>';
-                    echo $rpos;
+                    echo $uplace % 2;
 
                     break;
                 }
