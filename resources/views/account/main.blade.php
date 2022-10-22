0dc6caf898da46e0fa01be238f3b3ac9503bb94a
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-22 17:34:53 +0500

    Update main.blade.php

--- resources/views/account/main.blade.php
+++ resources/views/account/main.blade.php
@@ -128,6 +128,7 @@
 
             $uplace = 4;
 
+            $line_pay = 0;
             // Нужно ли двать деньги
             if( $uplace == $lineG[$current_line - 1] ){
                 if( $matrix_lvl == 1 ){
