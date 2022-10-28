4108c120b841aa3e1120c22d573ca9d877d4ec54
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-28 20:34:33 +0500

    Update adminPage.blade.php

--- resources/views/account/adminPage.blade.php
+++ resources/views/account/adminPage.blade.php
@@ -9,7 +9,7 @@
                 <h1 class="caninetName">Админ панель</h1>
                 <div class="cabMenuFlex displayFlex alignItemsCenter spaceBetween">
                     <a href="{{ route('account') }}" class="cabMenuLink{{ Route::currentRouteName() == 'account' ? ' active' : '' }}">Все пользователи</a>
-                    <a href="{{ route('start') }}" class="cabMenuLink{{ $disabledMunuLink }}{{ Route::currentRouteName() == 'start' ? ' active' : '' }}">Все пополнения</a>
+                    <a href="{{ route('start') }}" class="cabMenuLink{{ Route::currentRouteName() == 'start' ? ' active' : '' }}">Все пополнения</a>
                     {{-- <a href="{{ route('automation') }}" class="cabMenuLink{{ $disabledMunuLink }}{{ Route::currentRouteName() == 'automation' ? ' active' : '' }}">Автоматизация</a> --}}
                     <a href="{{ route('reviews') }}" class="cabMenuLink">Менеджер отзывов</a>
                     {{-- <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Настройки</a> --}}
