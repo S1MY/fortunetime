3d9fafc155194b06fee21a98db880dd90d01f2e0
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-28 22:20:16 +0500

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -109,7 +109,7 @@ class MainController extends Controller
     public function admin(){
 
         $users = DB::table('users')
-                    ->select('u2.login as sponsor_login', 'user_name' , 'user_surname', 'users.login', 'users.email', 'users.sponsor_counter', 'balance', 'activated', 'matrix_lvl', 'users.created_at')
+                    ->select('u2.login as sponsor_login', 'user_name' , 'user_surname', 'users.login', 'users.email', 'users.sponsor_counter', 'balance', 'activated', 'users.created_at')
                     ->leftJoin('users as u2', 'users.sponsor', '=', 'u2.id')
                     ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
                     ->get();
