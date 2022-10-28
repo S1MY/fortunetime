ae376372851dfbcb8695966341087a959a127901
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-28 20:34:06 +0500

    Update adminPage.blade.php

--- resources/views/account/adminPage.blade.php
+++ resources/views/account/adminPage.blade.php
@@ -6,8 +6,15 @@
     <section class="pageSection">
         <div class="container">
             <div class="pageInner">
-                @include('account.layout.accountMaster')
-                <p>Админка</p>
+                <h1 class="caninetName">Админ панель</h1>
+                <div class="cabMenuFlex displayFlex alignItemsCenter spaceBetween">
+                    <a href="{{ route('account') }}" class="cabMenuLink{{ Route::currentRouteName() == 'account' ? ' active' : '' }}">Все пользователи</a>
+                    <a href="{{ route('start') }}" class="cabMenuLink{{ $disabledMunuLink }}{{ Route::currentRouteName() == 'start' ? ' active' : '' }}">Все пополнения</a>
+                    {{-- <a href="{{ route('automation') }}" class="cabMenuLink{{ $disabledMunuLink }}{{ Route::currentRouteName() == 'automation' ? ' active' : '' }}">Автоматизация</a> --}}
+                    <a href="{{ route('reviews') }}" class="cabMenuLink">Менеджер отзывов</a>
+                    {{-- <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Настройки</a> --}}
+                    <a href="{{ route('logout') }}" class="cabMenuLink">Выйти</a>
+                </div>
             </div>
         </div>
     </section>
