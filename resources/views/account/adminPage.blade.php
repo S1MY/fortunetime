62437683e2c6cf39dce9071b18adb132ea747ecd
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-28 20:38:34 +0500

    adminmaster

--- resources/views/account/adminPage.blade.php
+++ resources/views/account/adminPage.blade.php
@@ -6,15 +6,9 @@
     <section class="pageSection">
         <div class="container">
             <div class="pageInner">
-                <h1 class="caninetName">Админ панель</h1>
-                <div class="cabMenuFlex displayFlex alignItemsCenter spaceBetween">
-                    <a href="{{ route('account') }}" class="cabMenuLink{{ Route::currentRouteName() == 'account' ? ' active' : '' }}">Все пользователи</a>
-                    <a href="{{ route('start') }}" class="cabMenuLink{{ Route::currentRouteName() == 'start' ? ' active' : '' }}">Все пополнения</a>
-                    <a href="{{ route('automation') }}" class="cabMenuLink{{ Route::currentRouteName() == 'automation' ? ' active' : '' }}">Добавить ответ</a>
-                    <a href="{{ route('reviews') }}" class="cabMenuLink">Менеджер отзывов</a>
-                    <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Новости</a>
-                    <a href="{{ route('logout') }}" class="cabMenuLink">Выйти</a>
-                </div>
+
+                @include('account.layout.adminMaster')
+
             </div>
         </div>
     </section>
