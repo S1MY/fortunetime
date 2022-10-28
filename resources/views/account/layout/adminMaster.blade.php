140bbfeaf5b532fe1a6e86964004f7b121072e57
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-29 03:12:47 +0500

    Update adminMaster.blade.php

--- resources/views/account/layout/adminMaster.blade.php
+++ resources/views/account/layout/adminMaster.blade.php
@@ -3,7 +3,7 @@
 <h1 class="caninetName">Админ панель</h1>
 <div class="cabMenuFlex displayFlex alignItemsCenter spaceBetween">
     <a href="{{ route('adminPage') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminPage' ? ' active' : '' }}">Все пользователи</a>
-    <a href="{{ route('paied') }}" class="cabMenuLink{{ Route::currentRouteName() == 'start' ? ' active' : '' }}">Все пополнения</a>
+    <a href="{{ route('paied') }}" class="cabMenuLink{{ Route::currentRouteName() == 'paied' ? ' active' : '' }}">Все пополнения</a>
     <a href="{{ route('automation') }}" class="cabMenuLink{{ Route::currentRouteName() == 'automation' ? ' active' : '' }}">Добавить FAQ</a>
     <a href="{{ route('reviews') }}" class="cabMenuLink">Менеджер отзывов</a>
     <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Новости</a>
