cb9730d6c4b3726d4b966a26b17e482f63247451
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-28 20:36:28 +0500

    Update adminPage.blade.php

--- resources/views/account/adminPage.blade.php
+++ resources/views/account/adminPage.blade.php
@@ -12,7 +12,7 @@
                     <a href="{{ route('start') }}" class="cabMenuLink{{ Route::currentRouteName() == 'start' ? ' active' : '' }}">Все пополнения</a>
                     <a href="{{ route('automation') }}" class="cabMenuLink{{ Route::currentRouteName() == 'automation' ? ' active' : '' }}">Добавить ответ</a>
                     <a href="{{ route('reviews') }}" class="cabMenuLink">Менеджер отзывов</a>
-                    {{-- <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Настройки</a> --}}
+                    <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Новости</a>
                     <a href="{{ route('logout') }}" class="cabMenuLink">Выйти</a>
                 </div>
             </div>
