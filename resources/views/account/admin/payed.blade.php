88a364c078997f2ae76842c25af08d1adc5e0661
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-29 03:02:38 +0500

    Update payed.blade.php

--- resources/views/account/admin/payed.blade.php
+++ resources/views/account/admin/payed.blade.php
@@ -9,19 +9,6 @@
 
                 @include('account.layout.adminMaster')
 
-                <h3 class="cabMatrixName" style="margin-top: 20px">Настройка вывода</h3>
-
-                <form id="formSorting" action="{{ route('adminSorting') }}">
-                    <input type="hidden" name="activated" id="activated" value="0">
-                    <input type="hidden" name="sponsor_login" id="sponsor_login" value="0">
-                    <input type="hidden" name="all" id="all" value="0">
-                </form>
-
-                <div class="cabMenuFlex displayFlex alignItemsCenter cabAdminMenu">
-                    <p class="adminBtn cursorPointer" data-sorting-name="activated">Активированные</p>
-                    <p class="adminBtn cursorPointer" data-sorting-name="sponsor_login">Поменять реферала</p>
-                    <p class="adminBtn cursorPointer" data-sorting-name="all">Показать всех</p>
-                </div>
                 <div class="tableWrapper">
                     <h3 class="cabMatrixName" style="margin-top: 20px">{{ $title }}</h3>
                     <table class="adminTable">
