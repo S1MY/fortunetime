0f1e97b7ec69387412d91cf06d2c8efe1b873f62
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-29 02:58:30 +0500

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -189,9 +189,10 @@ class MainController extends Controller
 
     public function paied(){
         $paieds = DB::table('freekassas')
-                    ->select('users.login', 'amount' , 'freekassas.created_at')
+                    ->select('users.login', 'sum(amount) as amount' , 'freekassas.created_at')
                     ->leftJoin('users', 'freekassas.user_id', '=', 'users.id')
                     ->where('status','=',1)
+                    ->groupBy('users.login')
                     ->get();
 
         $title = 'Все успешные пополнения ('.$paieds->count().')';
