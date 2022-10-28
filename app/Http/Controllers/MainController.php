5be6f1a5d474bbbb90006f2affd99d871f5c16f0
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-29 02:06:18 +0500

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -162,6 +162,14 @@ class MainController extends Controller
                     ->get();
         }
 
+        if( $request->all == 1 ){
+            $users = DB::table('users')
+                    ->select('u2.login as sponsor_login', 'user_name' , 'user_surname', 'users.login', 'users.email', 'users.sponsor_counter', 'balance', 'activated', 'users.created_at')
+                    ->leftJoin('users as u2', 'users.sponsor', '=', 'u2.id')
+                    ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
+                    ->get();
+        }
+
         return view('account.admin.usersTable', compact('users'));
     }
 }
