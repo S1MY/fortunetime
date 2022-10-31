94f9f3a5282b2d192ce4622ebca5df44de5d58bd
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-31 19:33:56 +0500

    Update adminMaster.blade.php

--- resources/views/account/layout/adminMaster.blade.php
+++ resources/views/account/layout/adminMaster.blade.php
@@ -5,7 +5,7 @@
     <a href="{{ route('adminPage') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminPage' ? ' active' : '' }}">Все пользователи</a>
     <a href="{{ route('paied') }}" class="cabMenuLink{{ Route::currentRouteName() == 'paied' ? ' active' : '' }}">Все пополнения</a>
     <a href="{{ route('adminfaq') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminfaq' ? ' active' : '' }}">Добавить FAQ</a>
-    <a href="{{ route('reviews') }}" class="cabMenuLink">Менеджер отзывов</a>
+    <a href="{{ route('adminreviews') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminreviews' ? ' active' : '' }}">Менеджер отзывов</a>
     <a href="{{ route('settings') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Новости</a>
     <a href="{{ route('logout') }}" class="cabMenuLink">Выйти</a>
 </div>
