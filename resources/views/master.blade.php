bf7b003493fb25c7e8d27484b9a3ec090eec935e
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-31 15:37:22 +0500

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="/css/main.css">
         <script src="/js/jquery-3.6.0.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+        {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet"> --}}
         <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
     </head>
     <body>
