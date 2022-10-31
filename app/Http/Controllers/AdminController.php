51c732f2bfcc228592aa28ff71047ca677f9ec28
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-31 17:01:43 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -96,26 +96,26 @@ class AdminController extends Controller
 
     public function adminAddFAQ(AdminRequest $request){
 
-        // if( $request->editID == 0 ){
-        //     DB::table('faq')->insert([
-        //         [
-        //             'qustion' => $request->question,
-        //             'answer' => $request->answer,
-        //             'created_at' => Carbon::now(),
-        //             'updated_at' => Carbon::now()
-        //         ],
-        //     ]);
-        // }else{
-        //     DB::table('faq')
-        //     ->where('id', $request->editID)
-        //     ->update([
-        //         [
-        //             'qustion' => $request->question,
-        //             'answer' => $request->answer,
-        //             'updated_at' => Carbon::now()
-        //         ],
-        //     ]);
-        // }
+        if( $request->editID == 0 ){
+            DB::table('faq')->insert([
+                [
+                    'qustion' => $request->question,
+                    'answer' => $request->answer,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+            ]);
+        }else{
+            DB::table('faq')
+            ->where('id', $request->editID)
+            ->update([
+                [
+                    'qustion' => $request->question,
+                    'answer' => $request->answer,
+                    'updated_at' => Carbon::now()
+                ],
+            ]);
+        }
 
         return $request->editID;
     }
