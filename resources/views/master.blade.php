5790b97afc5f435b28c5c16bb26e87fdb7b2470b
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-31 16:03:34 +0500

    Update master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -393,7 +393,6 @@
                 </div>
             </div>
         </div>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/lang/summernote-ru-RU.js"></script>
         <script src="/js/main.js"></script>
