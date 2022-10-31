6bed334a09e50ec46c57b4bc2023a95b1fe343ff
S1MY <71487094+S1MY@users.noreply.github.com>
2022-10-31 19:07:37 +0500

    Update faq.blade.php

--- resources/views/account/admin/faq.blade.php
+++ resources/views/account/admin/faq.blade.php
@@ -64,7 +64,7 @@
     <div class="popupResponse deleteQuestion">
         <div class="popupResponseBg"></div>
 
-        <form action="{{ route('faqDelete') }}" method="POST">
+        <form action="{{ route('faqDelete') }}" id="faqRemove" method="POST">
             <input type="hidden" name="removeid" id="removeid" value="">
         </form>
 
