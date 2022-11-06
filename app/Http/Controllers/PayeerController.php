7cf1d82eab1197461ada6d9415caadde3c8438c2
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-06 18:28:18 +0500

    Update PayeerController.php

--- app/Http/Controllers/PayeerController.php
+++ app/Http/Controllers/PayeerController.php
@@ -10,7 +10,8 @@ class PayeerController extends Controller
     public function pay(Request $request){
         $m_shop = '1770985667';
         $m_orderid = Auth::user()->id;
-        $m_amount = number_format($request->oa, 2, '.', '');
+        // $m_amount = number_format($request->oa, 2, '.', '');
+        $m_amount = number_format(10, 2, '.', '');
         $m_curr = 'RUB';
         $m_desc = base64_encode('Оплата одного из уровня маркетинга на проекте Fortune Time!');
         $m_key = 'DAJ12VfyfWmzQ5mu';
