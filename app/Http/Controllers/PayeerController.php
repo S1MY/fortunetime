37133efa887d0492569611c582a7b1878e332b37
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-06 18:42:13 +0500

    Update PayeerController.php

--- app/Http/Controllers/PayeerController.php
+++ app/Http/Controllers/PayeerController.php
@@ -2,11 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class PayeerController extends Controller
 {
+    public function payeer(Request $request){
+        if (!in_array($_SERVER['REMOTE_ADDR'], array('185.71.65.92', '185.71.65.189', '149.202.17.210'))) return;
+
+        if (isset($_POST['m_operation_id']) && isset($_POST['m_sign']))
+        {
+            $m_key = 'DAJ12VfyfWmzQ5mu';
+
+            $arHash = array(
+                $_POST['m_operation_id'],
+                $_POST['m_operation_ps'],
+                $_POST['m_operation_date'],
+                $_POST['m_operation_pay_date'],
+                $_POST['m_shop'],
+                $_POST['m_orderid'],
+                $_POST['m_amount'],
+                $_POST['m_curr'],
+                $_POST['m_desc'],
+                $_POST['m_status']
+            );
+
+            if (isset($_POST['m_params']))
+            {
+                $arHash[] = $_POST['m_params'];
+            }
+
+            $arHash[] = $m_key;
+
+            $sign_hash = strtoupper(hash('sha256', implode(':', $arHash)));
+
+            if ($_POST['m_sign'] == $sign_hash && $_POST['m_status'] == 'success')
+            {
+                DB::table('payeer')->insert([
+                    'user_id' => $_GET['m_orderid'],
+                    'amount' => explode('.', $_GET['m_amount'])[0],
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ]);
+
+                return true;
+
+            }
+
+            ob_end_clean(); exit($_POST['m_orderid'].'|error');
+        }
+    }
+
     public function pay(Request $request){
         $m_shop = '1770985667';
         $m_orderid = Auth::user()->id;
