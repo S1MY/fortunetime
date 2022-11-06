8d0c4c9914b4fadfb1cf24be7581ba1e096f2e44
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-06 18:09:03 +0500

    Update PayeerController.php

--- app/Http/Controllers/PayeerController.php
+++ app/Http/Controllers/PayeerController.php
@@ -22,20 +22,6 @@ class PayeerController extends Controller
             $m_desc
         );
 
-        /*
-        $arParams = array(
-            'success_url' => 'http://fortune-time.ru/new_success_url',
-            'fail_url' => 'http://fortune-time.ru/new_fail_url',
-            'status_url' => 'http://fortune-time.ru/new_status_url',
-        );
-
-        $key = md5('Ключ для шифрования дополнительных параметров'.$m_orderid);
-
-        $m_params = @urlencode(base64_encode(openssl_encrypt(json_encode($arParams), 'AES-256-CBC', $key, OPENSSL_RAW_DATA)));
-
-        $arHash[] = $m_params;
-        */
-
         $arHash[] = $m_key;
 
         $sign = strtoupper(hash('sha256', implode(':', $arHash)));
@@ -50,5 +36,9 @@ class PayeerController extends Controller
                 </form>';
 
         return $form;
+    }
+
+    public function payed(Request $request){
+        dd($request);
     }
 }
