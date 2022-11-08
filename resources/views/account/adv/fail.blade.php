8069b66e24dd1b91cf98ef0a2a9eac68b1291300
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-08 17:35:28 +0500

    adv curr fail

---
+++ resources/views/account/adv/fail.blade.php
@@ -0,0 +1,20 @@
+@extends('../master')
+
+@section('title', 'Автоматизация')
+
+@section('content')
+    <section class="pageSection">
+        <div class="container">
+            <div class="pageInner adminFaq" style="text-align: center;">
+                <img src="https://referalka24.ru/images/2021/12/04/advcash1.png" alt="advcash1">
+                <p>При оплате произошла ошибка.</p>
+                <a href="https://fortune-time.ru/account" class="subMenuItem active" style="justify-content: center; margin-top: 10px;">
+                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M15.6 10.4C16.9255 10.4 18 11.4745 18 12.8V13.6C18 16.7543 15.0248 20 10 20C4.97522 20 2 16.7543 2 13.6V12.8C2 11.4745 3.07452 10.4 4.4 10.4H15.6ZM10 0C12.4301 0 14.4 1.96995 14.4 4.4C14.4 6.83005 12.4301 8.8 10 8.8C7.56995 8.8 5.6 6.83005 5.6 4.4C5.6 1.96995 7.56995 0 10 0Z" fill="#202020"></path>
+                    </svg>
+                    <span>Вернуться в кабинет</span>
+                </a>
+            </div>
+        </div>
+    </section>
+@endsection
