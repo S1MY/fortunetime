33301d22b88375ab2bf9da26d2d7bb4856a707f2
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-15 22:26:51 +0500

    Update news.blade.php

--- resources/views/account/admin/news.blade.php
+++ resources/views/account/admin/news.blade.php
@@ -45,7 +45,7 @@
                                     <div class="newsText faqIDContent_{{ $new->id }}">{!!$new->content!!}</div>
                                 @endif
                                 @if ( $new->image != null )
-                                    <img src="{{$new->image}}" alt="news" class="newsImage faqIDImage_{{ $new->id }}">
+                                    <img src="{{storage_path($new->image)}}" alt="news" class="newsImage faqIDImage_{{ $new->id }}">
                                 @endif
                             </div>
                         @endforeach
