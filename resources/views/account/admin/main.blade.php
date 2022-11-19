80d30a0ff3bedc56df59df444ba291d597f38bf6
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-19 20:12:17 +0500

    Update main.blade.php

--- resources/views/account/admin/main.blade.php
+++ resources/views/account/admin/main.blade.php
@@ -76,8 +76,8 @@
 
             <form action="{{ route('changeUserReferal') }}" id="faqRemove" method="POST">
                 <input type="hidden" name="changeID" id="removeid" value="">
-                <input type="text" placeholder="Введите логин нового реферала">
-                <button>Изменить</button>
+                <input type="text" class="formInput" placeholder="Введите логин нового реферала">
+                <button  class="passBtn">Изменить</button>
             </form>
 
         </div>
