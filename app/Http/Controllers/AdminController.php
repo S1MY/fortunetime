dc8387aa69175b0204631bd02d64e6deac2adcdf
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-19 20:22:16 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -116,7 +116,7 @@ class AdminController extends Controller
 
     public function changeUserReferal(Request $request){
 
-        $sponsor = DB::table('users')->where('login', $request->newSponsor)->first();
+        $sponsor = DB::table('users')->where('login', '=', $request->newSponsor)->first();
 
         return $sponsor;
 
