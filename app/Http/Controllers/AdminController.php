01a8ceb3d0d679761399b62413b2923b7e0a47ae
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-19 20:10:49 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,7 +16,7 @@ class AdminController extends Controller
     public function admin(){
 
         $users = DB::table('users')
-                    ->select('u2.login as sponsor_login', 'user_name' , 'user_surname', 'users.login', 'users.email', 'users.sponsor_counter', 'balance', 'activated', 'users.created_at')
+                    ->select('u2.login as sponsor_login', 'user_id', 'user_name' , 'user_surname', 'users.login', 'users.email', 'users.sponsor_counter', 'balance', 'activated', 'users.created_at')
                     ->leftJoin('users as u2', 'users.sponsor', '=', 'u2.id')
                     ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
                     ->paginate(15);
