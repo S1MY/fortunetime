e7c8764b6cadc8e3a7557940142cc49d81183756
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-20 21:11:09 +0500

    Update tester.blade.php

--- resources/views/tester.blade.php
+++ resources/views/tester.blade.php
@@ -4,7 +4,7 @@
 
 
     // Заменить 10 на $user['id']
-    $user = DB::table('users')->where('id', 18)->first();
+    $user = DB::table('users')->where('id', 17)->first();
 
     // Берём спонсора и проверяем есть ли у него матрица
     $sp = $user->sponsor;
