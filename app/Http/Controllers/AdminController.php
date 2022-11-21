6e4aaec711173372b0198e5b00c88a7568c2afe3
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-21 22:10:46 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -120,7 +120,8 @@ class AdminController extends Controller
                                      'user_name',
                                      'user_surname',
                                      'avatar',
-                                     'email')
+                                     'email',
+                                     'created_at')
                             ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
                             ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
                             ->where('matrix_placers.referer_id', $matrix->matrix_id)
