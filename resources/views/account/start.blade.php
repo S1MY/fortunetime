5ef411ff6f53c19c6f7e52015a6b0d312cec7b68
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-23 17:01:23 +0500

    Update start.blade.php

--- resources/views/account/start.blade.php
+++ resources/views/account/start.blade.php
@@ -7,11 +7,8 @@
         <div class="container">
             <div class="pageInner">
                 @include('account.layout.accountMaster')
-                {{-- <p class="cabinetText">Система быстрого старта позволит вам пригласить минимум 10 партнёров в вашу команду. Даже без опыта. Всё что от вас требуется это соблюдать 4е простых шага, описанных в системе.</p>
-                <a href="#" class="cabinetBtn popupBtn" data-popup="starter">Скачать Систему</a> --}}
-                <p class="cabinetText" style="text-align: center">Старт маркетинг проекта будет 23.11.2022!
-                <br>    В данный наблюдаем за нагрузкой оказанной на сервер и на сайт!
-                <br>    Мы обязательно оповестим вас по EMail адрессу и в наших группах!</p>
+                <p class="cabinetText">Система быстрого старта позволит вам пригласить минимум 10 партнёров в вашу команду. Даже без опыта. Всё что от вас требуется это соблюдать 4е простых шага, описанных в системе.</p>
+                <a href="#" class="cabinetBtn popupBtn" data-popup="starter">Скачать Систему</a>
             </div>
         </div>
     </section>
