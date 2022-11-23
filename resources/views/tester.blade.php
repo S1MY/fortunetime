4cc5fa0e2ec7afed8046acd550bb09e5ae0868d8
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-23 17:16:22 +0500

    Update tester.blade.php

--- resources/views/tester.blade.php
+++ resources/views/tester.blade.php
@@ -4,7 +4,7 @@
 
 
     // Заменить 10 на $user['id']
-    $user = DB::table('users')->where('id', 3)->first();
+    $user = DB::table('users')->where('id', 22)->first();
 
     // Проверяем есть ли у нас матрица
 
