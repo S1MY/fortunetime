51d031f74f3d5d623bf7b4c99ef39c2accf5fc84
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-24 01:26:45 +0500

    Update MainController.php

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -81,28 +81,191 @@ class MainController extends Controller
 
     public function account(){
 
+        $user = DB::table('users')->where('id', '=', Auth::user()->id)->first();
+
         $matrix = DB::table('matrix')->where([
-                                        ['user_id', '=', Auth::user()->id],
-                                        ['matrix_lvl', '=', 1],
-                                    ])->first();
+                    ['user_id', '=', Auth::user()->id],
+                    ['matrix_lvl', '=', 1],
+                ])->first();
+
+        // Переменные если матрицы не существует
         $disabled = ' disabled';
         $matrixInfos = '';
         $matrixUsersCount = '';
 
-        // dd($matrix);
-
         if($matrix != null){
+
+            // Если матрица есть, показываем её
+
             $disabled = '';
+
+            // Берём людей из нашей матрицы
 
             $matrixInfos = DB::table('users')
                             ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
                             ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
-                            ->where('matrix_placers.matrix_id', $matrix->matrix_id)
-                            ->orWhere('matrix_placers.referer_id', $matrix->matrix_id)
+                            ->where([
+                                ['matrix_placers.matrix_id', $matrix->matrix_id],
+                                ['matrix_placers.line', 1],
+                            ])
                             ->get();
+
+            // Берём наших переливов
+            // И переименовываем поля, для слияния двух матриц
+
+            $matrixInfosReferers = DB::table('users')
+                            ->select('users.id',
+                                        'users.login',
+                                        'referer_id as matrix_id',
+                                        'referer_shoulder as shoulder',
+                                        'referer_line as line',
+                                        'referer_place as user_place',
+                                        'user_name',
+                                        'user_surname',
+                                        'avatar',
+                                        'email',
+                                        'matrix_placers.created_at')
+                            ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
+                            ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
+                            ->where([
+                                ['matrix_placers.referer_id', $matrix->matrix_id],
+                                ['matrix_placers.referer_line', 1]
+                            ])
+                            ->get();
+
+            // Склеиваем две коллекции
+
+            $matrixInfos = $matrixInfos->merge($matrixInfosReferers);
+            // dd($matrixInfos);
+
+            // Считаем кол-во людей в матрице на первом линии
+            $countMatrixMember = $matrixInfos->count();
+
+            // Берём людей, кого пригласили из первой линии
+
+            for ($i=0; $i < $countMatrixMember; $i++) {
+                // Берём айдишник и плечо в котором находится $i человек в нашей линии
+
+                $usID = $matrixInfos[$i]->id;
+                $usSholder = $matrixInfos[$i]->shoulder;
+
+                $UsMatrix = DB::table('matrix')->where([
+                                ['user_id', '=', $usID],
+                                ['matrix_lvl', '=', 1],
+                            ])->first();
+
+                $matrixInfosUs = DB::table('users')
+                            ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
+                            ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
+                            ->where([
+                                ['matrix_placers.matrix_id', $UsMatrix->matrix_id],
+                                ['matrix_placers.line', 1],
+                            ])
+                            ->take(2)
+                            ->get();
+
+                $matrixInfosUs->map(function($info) use ($usSholder){
+                    $info->line = $info->line + 1;
+                    $info->shoulder = $usSholder;
+
+                    return $info;
+                });
+
+                $countLineMatrixMebmer = $matrixInfosUs->count();
+
+
+
+                for ($m=0; $m < $countLineMatrixMebmer; $m++) {
+
+                    for ($d=1; $d < 8; $d++) {
+
+                        $usID2 = $matrixInfosUs[$m]->id;
+
+                        $usSholder = $matrixInfosUs[$m]->shoulder;
+
+                        $usLine = $matrixInfosUs[$m]->line;
+
+                        $UsMatrixLine = DB::table('matrix')->where([
+                                        ['user_id', '=', $usID2],
+                                        ['matrix_lvl', '=', 1],
+                                    ])->first();
+
+
+                        $matrixInfosUsLine = DB::table('users')
+                                    ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
+                                    ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
+                                    ->where([
+                                        ['matrix_placers.matrix_id', $UsMatrixLine->matrix_id],
+                                        ['matrix_placers.line', $d],
+                                    ])
+                                    ->take(2)
+                                    ->get();
+
+
+                        $matrixInfosUsLine->map(function($info) use ($usSholder, $usLine){
+                            $info->line = $usLine + 1;
+                            $info->shoulder = $usSholder;
+                            return $info;
+                        });
+
+                        $matrixInfosUs = $matrixInfosUs->merge($matrixInfosUsLine);
+                        $countLineMatrixMebmer = $matrixInfosUs->count();
+                    }
+
+                }
+
+                $matrixInfos = $matrixInfos->merge($matrixInfosUs);
+            }
+
+
+            // Берём наших людей и переливов по линиям
+
+            for ($i=2; $i < 8; $i++) {
+
+                $matrixID = $matrix->matrix_id;
+
+                $matrixInfosNext = DB::table('users')
+                            ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
+                            ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
+                            ->where([
+                                ['matrix_placers.matrix_id', $matrixID],
+                                ['matrix_placers.line', $i],
+                            ])
+                            ->get();
+
+                $matrixInfosReferersNext = DB::table('users')
+                            ->select('users.id',
+                                        'users.login',
+                                        'referer_id as matrix_id',
+                                        'referer_shoulder as shoulder',
+                                        'referer_line as line',
+                                        'referer_place as user_place',
+                                        'user_name',
+                                        'user_surname',
+                                        'avatar',
+                                        'email',
+                                        'matrix_placers.created_at')
+                            ->leftJoin('matrix_placers', 'users.id', '=', 'matrix_placers.user_id')
+                            ->leftJoin('user_infos', 'users.id', '=', 'user_infos.user_id')
+                            ->where([
+                                ['matrix_placers.referer_id', $matrixID],
+                                ['matrix_placers.referer_line', $i]
+                            ])
+                            ->get();
+
+                $matrixInfos = $matrixInfos->merge($matrixInfosNext);
+                $matrixInfos = $matrixInfos->merge($matrixInfosReferersNext);
+
+            }
 
             $matrixUsersCount = $matrixInfos->count();
         }
+
+        $user_max_lvl = DB::table('matrix')->where([
+            ['user_id', '=', $user->id],
+        ])
+        ->orderBy('matrix_lvl', 'DESC')
+        ->first();
 
         return view('account.main', compact('matrix', 'disabled', 'matrixInfos', 'matrixUsersCount'));
     }
