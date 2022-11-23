f75fceaab154b60719332d29a3ff1eea9d5b214e
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-23 19:23:53 +0500

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -217,7 +217,7 @@ class AdminController extends Controller
                 $countLineMatrixMebmer = $matrixInfosUs->count();
             }
 
-            dd($countLineMatrixMebmer);
+            // dd($countLineMatrixMebmer);
 
             // Берём наших людей и переливов по линиям
 
