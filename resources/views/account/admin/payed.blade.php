a94b713444d99d46d178bff510864b8834b68be5
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-24 14:08:46 +0500

    Update payed.blade.php

--- resources/views/account/admin/payed.blade.php
+++ resources/views/account/admin/payed.blade.php
@@ -9,37 +9,43 @@
 
                 @include('account.layout.adminMaster')
 
-                <div class="bgBlue" style="color: #FFFFFF;
-                                           background: #4168D2;
-                                           display: inline-block;
-                                           border-radius: 6px;
-                                           margin-top: 10px;
-                                           padding: 10px 18px;">
-                    <h3>Общая сумма</h3>
-                    <p style="font-size: 24px;
-                              font-weight: bold;">{{ $paiedsum }} руб.</p>
-                </div>
+                @if ($paiedsum == 0)
+                    <h3 class="cabMatrixName" style="margin-top: 20px">Пополнений ещё не было</h3>
+                @else
+                    <div class="bgBlue" style="color: #FFFFFF;
+                                            background: #4168D2;
+                                            display: inline-block;
+                                            border-radius: 6px;
+                                            margin-top: 10px;
+                                            padding: 10px 18px;">
+                        <h3>Общая сумма</h3>
+                        <p style="font-size: 24px;
+                                font-weight: bold;">{{ $paiedsum }} руб.</p>
+                    </div>
 
-                <div class="tableWrapper">
-                    <h3 class="cabMatrixName" style="margin-top: 20px">{{ $title }}</h3>
-                    <table class="adminTable">
-                        <tr>
-                            <th>Логин</th>
-                            <th>Сумма</th>
-                            <th>Дата</th>
-                        </tr>
-                        @foreach ($paieds as $paied)
+                    <div class="tableWrapper">
+                        <h3 class="cabMatrixName" style="margin-top: 20px">{{ $title }}</h3>
+                        <table class="adminTable">
                             <tr>
-                                <td class="tacenter">{{ $paied->login }}</td>
-                                <td class="tacenter">{{ $paied->amount }}</td>
-                                <td class="tacenter">{{ $paied->created_at }}</td>
+                                <th>Логин</th>
+                                <th>Сумма</th>
+                                <th>Дата</th>
                             </tr>
-                        @endforeach
+                            @foreach ($paieds as $paied)
+                                <tr>
+                                    <td class="tacenter">{{ $paied->login }}</td>
+                                    <td class="tacenter">{{ $paied->amount }}</td>
+                                    <td class="tacenter">{{ $paied->created_at }}</td>
+                                </tr>
+                            @endforeach
 
-                    </table>
+                        </table>
 
-                    {{ $paieds->links('vendor.pagination.default') }}
-                </div>
+                        {{ $paieds->links('vendor.pagination.default') }}
+                    </div>
+                @endif
+
+
             </div>
         </div>
     </section>
