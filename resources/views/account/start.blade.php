42d5c8f3eb4fc235c558091938260a0641744982
S1MY <71487094+S1MY@users.noreply.github.com>
2022-11-24 09:26:24 +0500

    Update start.blade.php

--- resources/views/account/start.blade.php
+++ resources/views/account/start.blade.php
@@ -9,9 +9,9 @@
                 @include('account.layout.accountMaster')
                 <p class="cabinetText">Система быстрого старта позволит вам пригласить минимум 10 партнёров в вашу команду. Даже без опыта. Всё что от вас требуется это соблюдать 4е простых шага, описанных в системе.</p>
                 @if (Auth::user()->UserInfo->activated == 1)
-                    <a href="#" class="cabinetBtn popupBtn" data-popup="starter">Скачать Систему</a>
-                @else
                     <a href="Система ФОРТУНА.pdf" class="cabinetBtn popupBtn" download>Скачать Систему</a>
+                @else
+                    <a href="#" class="cabinetBtn popupBtn" data-popup="starter">Скачать Систему</a>
                 @endif
             </div>
         </div>
