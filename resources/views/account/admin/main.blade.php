24841df3fd622dba01a8811d733018bdaa715a10
S1MY <71487094+S1MY@users.noreply.github.com>
2022-12-06 15:53:48 +0500

    ad

--- resources/views/account/admin/main.blade.php
+++ resources/views/account/admin/main.blade.php
@@ -8,20 +8,22 @@
             <div class="pageInner">
 
                 @include('account.layout.adminMaster')
+                <div class="outSettings">
+                    <h3 class="cabMatrixName" style="margin-top: 20px">Настройка вывода</h3>
 
-                <h3 class="cabMatrixName" style="margin-top: 20px">Настройка вывода</h3>
+                    <form id="formSorting" action="{{ route('adminSorting') }}">
+                        <input type="hidden" name="activated" id="activated" value="0">
+                        <input type="hidden" name="sponsor_login" id="sponsor_login" value="0">
+                        <input type="hidden" name="all" id="all" value="0">
+                    </form>
 
-                <form id="formSorting" action="{{ route('adminSorting') }}">
-                    <input type="hidden" name="activated" id="activated" value="0">
-                    <input type="hidden" name="sponsor_login" id="sponsor_login" value="0">
-                    <input type="hidden" name="all" id="all" value="0">
-                </form>
-
-                <div class="cabMenuFlex displayFlex alignItemsCenter cabAdminMenu">
-                    <p class="adminBtn cursorPointer" data-sorting-name="activated">Активированные</p>
-                    <p class="adminBtn cursorPointer" data-sorting-name="sponsor_login">Поменять реферала</p>
-                    <p class="adminBtn cursorPointer" data-sorting-name="all">Показать всех</p>
+                    <div class="cabMenuFlex displayFlex alignItemsCenter cabAdminMenu">
+                        <p class="adminBtn cursorPointer" data-sorting-name="activated">Активированные</p>
+                        <p class="adminBtn cursorPointer" data-sorting-name="sponsor_login">Поменять реферала</p>
+                        <p class="adminBtn cursorPointer" data-sorting-name="all">Показать всех</p>
+                    </div>
                 </div>
+
                 <div class="tableWrapper">
                     <h3 class="cabMatrixName" style="margin-top: 20px">{{ $title }}</h3>
                     <table class="adminTable">
