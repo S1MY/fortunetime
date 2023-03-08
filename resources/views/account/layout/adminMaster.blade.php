9fed0b34321f825836c304eadf94b35786bbe061
S1MY <71487094+S1MY@users.noreply.github.com>
2023-03-08 14:16:38 +0500

    Update adminMaster.blade.php

--- resources/views/account/layout/adminMaster.blade.php
+++ resources/views/account/layout/adminMaster.blade.php
@@ -4,9 +4,9 @@
 <div class="cabMenuFlex displayFlex alignItemsCenter spaceBetween">
     <a href="{{ route('adminPage') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminPage' ? ' active' : '' }}">Все пользователи</a>
     <a href="{{ route('paied') }}" class="cabMenuLink{{ Route::currentRouteName() == 'paied' ? ' active' : '' }}">Все пополнения</a>
+    <a href="{{ route('adminoutput') }}" class="cabMenuLink{{ Route::currentRouteName() == 'output' ? ' active' : '' }}">Заявки на вывод</a>
     <a href="{{ route('adminfaq') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminfaq' ? ' active' : '' }}">Добавить FAQ</a>
     <a href="{{ route('adminreviews') }}" class="cabMenuLink{{ Route::currentRouteName() == 'adminreviews' ? ' active' : '' }}">Менеджер отзывов</a>
     <a href="{{ route('adminnews') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Новости</a>
-    <a href="{{ route('adminoutput') }}" class="cabMenuLink{{ Route::currentRouteName() == 'settings' ? ' active' : '' }}">Новости</a>
     <a href="{{ route('logout') }}" class="cabMenuLink">Выйти</a>
 </div>
