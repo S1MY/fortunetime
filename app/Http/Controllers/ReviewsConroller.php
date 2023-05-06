3af688451eafe28d80551b79ae09fadde5722657
S1MY <71487094+S1MY@users.noreply.github.com>
2023-05-06 14:36:00 +0500

    Update ReviewsConroller.php

--- app/Http/Controllers/ReviewsConroller.php
+++ app/Http/Controllers/ReviewsConroller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ReviewsRequest;
 use App\Models\Reviews;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\DB;
 
 class ReviewsConroller extends Controller
 {
@@ -15,7 +16,7 @@ class ReviewsConroller extends Controller
             'user_id' => $request['user_id'],
             'review' => $request['review'],
         ]);
-
-        return $Reviews;
+        $dbname = DB::connection()->getDatabaseName();
+        return "Connected successfully to the database. Database name is :".$dbname;
     }
 }
