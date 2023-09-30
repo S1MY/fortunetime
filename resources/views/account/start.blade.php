c6c15bfcd4f8df0cccd9e40c0f8cc8b690ac4908
S1MY <71487094+S1MY@users.noreply.github.com>
2023-09-30 13:56:04 +0500

    Update start.blade.php

--- resources/views/account/start.blade.php
+++ resources/views/account/start.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'Быстрый старт')
 
 @section('content')
+    <style>
+        .dFlex{
+            display: inline-flex;
+            justify-content: space-between;
+        }
+    </style>
     <section class="pageSection">
         <div class="container">
             <div class="pageInner">
@@ -12,24 +18,17 @@
                     width="480"
                     controls
                     poster="https://archive.org/download/WebmVp8Vorbis/webmvp8.gif">
-                    <source
-                        src="https://archive.org/download/WebmVp8Vorbis/webmvp8_512kb.mp4"
-                        type="video/mp4" />
-                    <source
-                        src="https://archive.org/download/WebmVp8Vorbis/webmvp8.ogv"
-                        type="video/ogg" />
-                    <source
-                        src="https://archive.org/download/WebmVp8Vorbis/webmvp8.webm"
-                        type="video/webm" />
                     Your browser doesn't support HTML5 video tag.
                     </video>
+                    <div class="text">
+                        <p class="cabinetText">Система быстрого старта позволит вам пригласить минимум 10 партнёров в вашу команду. Даже без опыта. Всё что от вас требуется это соблюдать 4е простых шага, описанных в системе.</p>
+                        @if (Auth::user()->UserInfo->activated == 1)
+                            <a href="Система ФОРТУНА.pdf" class="cabinetBtn" download>Скачать Систему</a>
+                        @else
+                            <a href="#" class="cabinetBtn popupBtn" data-popup="starter">Скачать Систему</a>
+                        @endif
+                    </div>
                 </div>
-                <p class="cabinetText">Система быстрого старта позволит вам пригласить минимум 10 партнёров в вашу команду. Даже без опыта. Всё что от вас требуется это соблюдать 4е простых шага, описанных в системе.</p>
-                @if (Auth::user()->UserInfo->activated == 1)
-                    <a href="Система ФОРТУНА.pdf" class="cabinetBtn" download>Скачать Систему</a>
-                @else
-                    <a href="#" class="cabinetBtn popupBtn" data-popup="starter">Скачать Систему</a>
-                @endif
             </div>
         </div>
     </section>
