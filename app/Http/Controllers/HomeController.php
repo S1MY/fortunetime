654a317c698b0fcff3f35006565531c6680166cc
S1MY <71487094+S1MY@users.noreply.github.com>
2023-10-22 12:21:00 +0500

    da

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $pwd = Hash::make('1234');
+        return view('home', compact('pwd'));
     }
 }
