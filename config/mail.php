bf00208013f2ca7548dec6616f21930b79487652
S1MY <71487094+S1MY@users.noreply.github.com>
2023-12-03 12:33:39 +0500

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -92,8 +92,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'support@fortune-time.su'),
+        'name' => env('MAIL_FROM_NAME', 'Техническая поддержка fortune-time.su'),
     ],
 
     /*
